5d76db6bbc9fb1cb4a07faf5f31a7b9c3c796973
Mohamed Elgammal <mohamed.elgammal@mail.utoronto.ca>
2020-04-19 03:05:42 -0400

    update the feasible move generator to choos from the highly critical connections

--- vpr/src/place/timing_place.h
+++ vpr/src/place/timing_place.h
@@ -7,6 +7,8 @@
 #include "place_delay_model.h"
 #include "vpr_net_pins_matrix.h"
 
+const int HIGH_CRIT = 0.95;
+extern std::vector<std::pair<ClusterNetId, int>> highly_crit_pins;
 std::unique_ptr<PlaceDelayModel> alloc_lookups_and_criticalities(t_chan_width_dist chan_width_dist,
                                                                  const t_placer_opts& place_opts,
                                                                  const t_router_opts& router_opts,
